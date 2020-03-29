d7d02e003806c9d5fc9f0a4f8b79f0e99d82c19e
Zoe McCarthy <zoemccarthy12@gmail.com>
2020-03-28 23:21:15 -0500

    feat: Added artist insertion from the create_artist form.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -472,13 +472,31 @@ def create_artist_form():
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
     # called upon submitting the new artist listing form
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
+    try:
+        form = request.form
+        artist = Artist(
+            name=form['name'],
+            city=form['city'],
+            state=form['state'],
+            phone=form['phone'],
+            genres=str(form.getlist('genres')),  # can have multiple here, store them as a string of the list
+            facebook_link=form['facebook_link']
+        )
+        data = artist
+        db.session.add(artist)
+        db.session.commit()
+        # on successful db insert, flash success
+        flash('Artist ' + data.name + ' was successfully listed!')
+    except:
+        db.session.rollback()
+        error = True
+        print(sys.exc_info())
+        # on unsuccessful db insert, flash an error instead.
+        flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+    finally:
+        db.session.close()
 
     # on successful db insert, flash success
-    flash('Artist ' + request.form['name'] + ' was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
     return render_template('pages/home.html')
 
 
